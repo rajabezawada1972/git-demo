fb89e874814bf7b60215aa7eb580efb24d888f5a
rajabezawada1972 <62781911+rajabezawada1972@users.noreply.github.com>
2020-03-28 11:15:17 +0530

    Update Demo.java

--- src/main/java/com/raja/Demo.java
+++ src/main/java/com/raja/Demo.java
@@ -5,10 +5,14 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		m1();
+		m2();
 	}
 	
 	public static void m1() {
 		System.out.println("inside static method");
+	}
+	public static void m2(){
+		System.out.println("inside second method");
 	}
 
 }
