5d4e4934ce5c57058543dbb1fbb2c0c83871ff16
rajabezawada <raja.java1972@gmail.com>
2020-03-28 11:12:01 +0530

    added classes and changes

---
+++ src/main/java/com/raja/Demo.java
@@ -0,0 +1,14 @@
+package com.raja;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		m1();
+	}
+	
+	public static void m1() {
+		System.out.println("inside static method");
+	}
+
+}
