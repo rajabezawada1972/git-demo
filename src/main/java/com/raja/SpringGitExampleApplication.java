2be2036c3e29bb41776d40b4982f955daf43a73b
rajabezawada <raja.java1972@gmail.com>
2020-03-28 10:17:46 +0530

    git demo project

---
+++ src/main/java/com/raja/SpringGitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.raja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitExampleApplication.class, args);
+	}
+
+}
