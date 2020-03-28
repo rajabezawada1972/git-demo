1db097a813daff621d1689cc15f2e314b9efcbc4
rajabezawada <raja.java1972@gmail.com>
2020-03-28 10:21:06 +0530

    changes on git demo

--- src/main/java/com/raja/SpringGitExampleApplication.java
+++ src/main/java/com/raja/SpringGitExampleApplication.java
@@ -2,9 +2,16 @@ package com.raja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringGitExampleApplication {
+	
+	@GetMapping("/getData")
+	public String getData() {
+		System.out.println("________________-");
+		return "dummy Data";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitExampleApplication.class, args);
